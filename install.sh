a663b7a700b671aabe63e12de01a982b4448396a
Fitorec <chanerec@gmail.com>
2012-03-21 20:16:59 -0600

    Add install file

--- install.sh
+++ install.sh
@@ -10,13 +10,14 @@ function installAire() {
 	export aire_dst=$1
 	if [ "${aire_dst}" = "" ]
 	then
-		export aire_dst=${HOME}/.config/aire/
+		export aire_dst=${HOME}/.config/
 	fi
 	echo "ruta de instalacion ${aire_dst}";
 	#descargamos el aire en el directorio temporal
 	wget https://github.com/r00tw33d/Aire/tarball/master -O /tmp/aire.tar.gz
 	tar xvfz /tmp/aire.tar.gz -C ${aire_dst}
-
+	rename 's/\*/aire/' "${aire_dst}/r00tw33d-Aire*"
+	
 	cat ${HOME}/.bashrc | grep -qE 'alias\s+aire='
 
 	if [ $? -ne 0 ]
@@ -27,7 +28,9 @@ function installAire() {
 
 
 wget https://github.com/r00tw33d/Aire/tarball/master -O aire.tar.gz
-tar xvfz aire.tar.gz -C ./aire/
+tar xvfz aire.tar.gz
+rename 's/\*/aire/' r00tw33d-Aire*
+
 echo 'pendiente por favor ejecute: ';
 echo 'sudo ./aire/aire.sh';
 exit 0;
